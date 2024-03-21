73b9c102480afe45c6c243b9551e4f4eecaa05bd
verloren <danil.koval.32000@gmail.com>
2024-03-21 12:01:53 +0400

    dynamic problem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,8 @@ interface HomeProps {
 	searchParams: IListingsParams
 }
 
+export const dynamic = 'force-dynamic'
+
 const Home = async ({ searchParams }: HomeProps) => {
 
 	const listings = await getListings(searchParams);
